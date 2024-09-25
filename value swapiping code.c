8aaf71f979ff389fd689fc4af52a979636f0cf57
Light54321 <99309736+Light54321@users.noreply.github.com>
2024-09-25 10:08:05 +0530

    Update value swapiping code.c

--- value swapiping code.c
+++ value swapiping code.c
@@ -1,18 +1,12 @@
  #include<stdio.h>
  void main( )
 {
- float basicsalaray,DA,HRA,Fund,grosssalary,netsalaray;
- printf("Please enter your basic salaray=");
- scanf("%f",&basicsalaray);
- printf("Your basic salaray %f",basicsalaray);
- DA=(basicsalaray)*(0.25);
- printf("Your DA is %f\n",DA);
- HRA=(basicsalaray)*(0.15);
- printf("Your HRA is %f\n",HRA);
- grosssalary=basicsalaray+DA+HRA;
- printf("Your gross salary is %f\n",grosssalary);
- Fund=grosssalary*(0.1);
- printf("Your Fund is %f\n",Fund);
- netsalaray=basicsalaray+DA+HRA-Fund;
- printf("Your Net salary is %f\n",netsalaray);
+ int a,b,temp;
+ printf("Pleas enter the value of a and b");
+ scanf("%d%d",&a,&b);
+ printf("before swapping th value of a and value of b is%d%d",a,b);
+ temp=a;
+ a=b;
+ b=temp;
+ printf("after the swapping the value of a is and value of b is%d%d",a,b);
 }

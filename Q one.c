eab3e2511560fecea9deec108f1d350ce3729cab
Light54321 <99309736+Light54321@users.noreply.github.com>
2024-09-25 08:51:43 +0530

    Add files via upload

---
+++ Q one.c
@@ -0,0 +1,13 @@
+ #include<stdio.h>
+ void main( )
+{
+ float maths, english, chemistery,physics,cs, sum, average,percentage ;
+printf("Enter the marks of maths, english, chemistery, physics,cs");
+ scanf("%f%f%f%f%f",&maths,&english, &chemistery, &physics,&cs);
+ sum=maths+english+chemistery+physics+cs;
+ printf("the sum of all the subject is=%f",sum);
+ average=sum/5;
+printf("the average of all subject is=%f",average);
+percentage=(sum/500)*100;
+printf("The percentage of the student in mid term is= %f",percentage);
+}
